622dec75368e5e8bcbdf108d1afd99e96cdc5371
Luan Braz <luan_silvacpv@hotmail.com>
2023-08-29 08:54:52 -0300

    Filtro JWT autentica rotas, valida tokens, e define usuário no Spring Security + Classe JwtUtil gera e valida tokens JWT na API, usando reivindicações e chave secreta. + SecurityConfig: classe configura o Spring Security na API. Define autenticação personalizada, desabilita CSRF, permite acesso público a algumas rotas, exige autenticação em outras, configura CORS e usa autenticação baseada em token JWT.

---
+++ salesmanagement-api/src/main/java/com/vivahlinda/salesmanagement/JWT/JwtFilter.java
@@ -0,0 +1,74 @@
+package com.vivahlinda.salesmanagement.JWT;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private CustomerUsersDetailsService service;
+
+    Claims claims = null;
+    private String nomeUsuario = null;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        if (httpServletRequest.getServletPath().matches(("/usuario/login|/usuario/forgotPassword|/usuario/signup"))) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        } else {
+
+            String authorizationHeader = httpServletRequest.getHeader("Authorization");
+            String token = null;
+
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                token = authorizationHeader.substring(7);
+                nomeUsuario = jwtUtil.extrairNomeUsuario(token);
+                claims = jwtUtil.extractAllClaims(token);
+            }
+
+            if (nomeUsuario != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = service.loadUserByUsername(nomeUsuario);
+                if (jwtUtil.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(httpServletRequest)
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        }
+    }
+
+    // Método que verifica se o usuário autenticado é um administrador.
+    public boolean isAdmin() {
+        return "admin".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    // Método que verifica se o usuário autenticado é um usuário comum.
+    public boolean isUsuario() {
+        return "usuario".equalsIgnoreCase((String) claims.get("role"));
+    }
+
+    // Método que retorna o nome do usuário atualmente autenticado.
+    public String getUsuarioAtual() {
+        return nomeUsuario;
+    }
+}
